5f591af75bfc84d8644658afd73880ccdf0e36da
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2017-07-11 21:43:04 -0400

    installer changes

--- installer.js
+++ installer.js
@@ -1,59 +1,75 @@
 //Package Installer Exercise
 
 //input array
-var inputArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: Leetmeme', 'Ice: '];
+var inputArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: ', 'Ice: Leetmeme'];
 //temp string to hold dependencies and packages
 var tempPackAndDep = [];
-//output string
+
+//output string 
 var output = [];
 
-var Toposort = require('toposort-class'),
-    t = new Toposort();
-
 //package installer to execute
-function packageInstaller(input) {
+function packageInstaller (input) {
     
-    //try first toposort-class package, see if it works...
-    //code goes here
-
-    t.add('KittenService', ' ').add('Leetmeme', 'Cyberportrat');    
-
     //loop through input array split the element to package and dependency if there is one
-    //add to output packages without dependency first
-    //add dependency before package to a separate from output array
+    //add to output packages without dependency first, add dependency before package if there is a dependency
     for (var i=0; i < input.length; i++) {
 
-        var tempArray = input[i].trim().split(':');
-        var tempPackage = tempArray[0].trim();
-        var tempDependency = tempArray[1].trim();
+       var tempArray = input[i].trim().split(':');
+       var tempPackage = tempArray[0].trim();
+       var tempDependency = tempArray[1].trim();
 
-        //check if package has dependency if not add to the output
-        if(tempArray.length != 0) {
+       //check if package has dependency if not add to the output
+       if(tempArray.length != 0) {
             if(tempDependency === '') {
 
                 if(output.indexOf(tempPackage) === -1) {
                     output.push(tempPackage);
                 }
             };
-        };
-        //console.log(tempDependency);
 
-    //if package has dependency and it's not in the output, add to output
-
-    //if package already there log an error if not add dependency and the package to the output
-
+         };
     };
 
-    //merge package and package with dependencies arrays
+    //console.log(output);
 
-    //print the output result
-    console.log(output);
+    for (var i=0; i < input.length; i++) {
 
-    console.log(t.sort().reverse());
+        var tempArray = input[i].trim().split(':');
+        var tempPackage = tempArray[0].trim();
+        var tempDependency = tempArray[1].trim();
 
+        //if package has dependency and it's not in the output, add to output
+        if (tempDependency != '') {
+            //if package already in the array and dependency not log an error 
+            if(tempPackAndDep.indexOf(tempPackage) > -1 && tempPackAndDep.indexOf(tempDependency) != -1 ) {
+                console.log('Cycle error - package: ' + tempPackage + ', dependency: ' + tempDependency);
+            }
+
+            if(tempPackAndDep.indexOf(tempDependency) === -1) {
+                tempPackAndDep.push(tempDependency);
+            }
+
+            if(tempPackAndDep.indexOf(tempPackage) === -1) {
+                    tempPackAndDep.push(tempPackage);
+            }
+        };
+    };
+
+    if(tempPackAndDep.indexOf(tempPackage) > -1 && tempPackAndDep.indexOf(tempDependency) != -1 ) {
+        return;
+    }
+    else {
+        //merge package and package with dependencies arrays
+        output = output.concat(tempPackAndDep);
+
+        var noDupe = Array.from(new Set(output));
+        
+        //print the output result
+        console.log(noDupe);
+    }
 };
 
 //run the package installer
 packageInstaller(inputArray);
 
- 
