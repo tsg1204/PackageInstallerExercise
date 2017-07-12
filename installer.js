8892e36f0aec8bb23e8d377bef3b2e12732f4552
Tatiana <tsg1204@gmail.com>
2017-07-12 11:57:33 -0400

    test fixed

--- installer.js
+++ installer.js
@@ -1,52 +1,43 @@
 //Package Installer Exercise
-
-//input array
 var inputArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: Leetmeme', 'Ice: '];
 
-//temp string to hold dependencies and packages
-var tempPackAndDep = [];
+var invalidArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: ', 'Ice: Leetmeme'];
 
-//output string 
-var output = [];
+//package installer 
+var packageInstaller;
 
-//cycle error
-var cycleError = false;
+packageInstaller = {
+  installPackage: function(input) {
+    //temp string to hold dependencies and packages
+    var tempPackAndDep = [];
 
-//package installer to execute
-function packageInstaller (input) {
-    
-    //loop through input array get packages without dependency
+    //output string 
+    var output = [];    
+
+    if (!Array.isArray(input))
+      throw new Error('Input is not an array');  
+
+    if (input.length === 0)
+        throw new Error('Input contains 0 packages');  
+
     for (var i=0; i < input.length; i++) {
-
         var tempArray = input[i].trim().split(':');
         var tempPackage = tempArray[0].trim();
         var tempDependency = tempArray[1].trim();
 
-        //check if package has dependency if not add to the output
-        if(tempArray.length != 0) {
-            if(tempDependency === '') {
+        //add packages without dependencies to the output first
+        if(tempDependency === '') {
 
-                if(output.indexOf(tempPackage) === -1) {
-                    output.push(tempPackage);
-                }
-            };
-
+            if(output.indexOf(tempPackage) === -1) {
+                output.push(tempPackage);
+            }
         };
-    };
-    //split input array, get only elements where package has dependency
-    for (var i=0; i < input.length; i++) {
-        var tempArray = input[i].trim().split(':');
-        var tempPackage = tempArray[0].trim();
-        var tempDependency = tempArray[1].trim();
 
-        cycleError = false;
         //if package has dependency and it's not in the output, add to output
         if (tempDependency != '') {
-            //if package already in the array and dependency not log an error 
+            //if package already in the otput but it has a dependency which is not - cycle error 
             if(tempPackAndDep.indexOf(tempPackage) > -1 && tempPackAndDep.indexOf(tempDependency) != -1 ) {
-                console.log('Cycle error - package: ' + tempPackage + ', dependency: ' + tempDependency);
-                cycleError = true;
-                return;
+                throw new Error('Cycle error - package: ' + tempPackage + ', dependency: ' + tempDependency);
             }
 
             if(tempPackAndDep.indexOf(tempDependency) === -1) {
@@ -59,20 +50,23 @@ function packageInstaller (input) {
         };
     };
 
-    if(cycleError) {
-        return;
-    }
-    else {
-        //merge package and package with dependencies arrays
-        output = output.concat(tempPackAndDep);
+    //merge package and package with dependencies array
+    output = output.concat(tempPackAndDep);
 
-        var noDupe = Array.from(new Set(output));
-        
-        //print the output result
-        console.log(noDupe);
-    }
+    var noDupe = Array.from(new Set(output));
+    
+    //print the output result
+    console.log(noDupe);
+    return noDupe;
+  }
 };
 
 //run the package installer
-packageInstaller(inputArray);
+packageInstaller.installPackage(inputArray);
+//packageInstaller.installPackage(invalidArray);
+//packageInstaller.installPackage(15);
+//packageInstaller.installPackage([]);
+
+module.exports = packageInstaller;
+
 
--- test/test.js
+++ test/test.js
@@ -1,14 +1,47 @@
-var assert = require("assert");
-var Installer = require("../installer");
+var installer = require("../installer");
 var chai = require("chai");
 var expect = chai.expect;
-var should = chai.should();
-//var sinon = require("sinon");
 
 describe("Package Installer", function() {
+  it('1: Valid input test', function() {
 
-  //invalid input array
-  var invalidArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: ', 'Ice: Leetmeme'];
+    //valid input 
+    var inputArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: Leetmeme', 'Ice: '];
+    var expectedOutput = [ 'KittenService', 'Ice', 'Cyberportrat', 'Leetmeme', 'CamelCaser', 'Fraudstream' ];
+    var result = installer.installPackage(inputArray);
 
-  
-})
+    expect(result).to.deep.equal(expectedOutput);   
+
+  });
+  it('2: Invalid input array test', function() {
+
+    //invalid input array
+    var invalidArray = ['KittenService: ', 'Leetmeme: Cyberportrat', 'Cyberportrat: Ice', 'CamelCaser: KittenService', 'Fraudstream: ', 'Ice: Leetmeme'];
+    var errorFunc = function() {
+    	installer.installPackage(invalidArray);
+    }
+
+    expect(errorFunc).to.throw();
+
+  });  
+  it('3: Not array input test', function() {
+
+    var value = 45;
+    var errorFunc = function() {
+    	installer.installPackage(value);
+    }
+
+    expect(errorFunc).to.throw();
+
+  });
+  it('4: Empty array input test', function() {
+
+    var invalidArray = [];
+    var errorFunc = function() {
+    	installer.installPackage(invalidArray);
+    }
+
+    expect(errorFunc).to.throw();
+
+  });
+});
